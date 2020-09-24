115f19e6f2bd9d43e14a8b8781e042f9de690f4c
KalanaPasindu <Kpasindudekostha96@gmail.com>
2020-09-25 04:59:30 +0530

    Added

--- Stash.java
+++ Stash.java
@@ -1,2 +1,38 @@
 public class StashCommand
-{}
+{
+  String getStashId()
+	   {
+	   return stashId;
+	   }
+	   String getStashName()
+	   {
+	   return stashName;
+	   }
+	   String getStashMessg()
+	   {
+	   return stashMessg;
+	   }
+
+	   void setStashId(int i)
+	   {
+         	   stashId =i;
+	   }
+	   void setStashName(int n)
+	   {
+         	   stashName =n;
+	   }
+	   void setStashMessg(int m)
+	   {
+         	   stashMessg =m;
+	   }
+
+	   void AddStash( )
+	   {
+	   }
+	   void removeStash()
+	   {
+	   }
+	   void clearAllStash()
+	   {
+	   }
+}
